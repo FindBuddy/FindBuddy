d27d329a2deb4108da4f4f9bd3dae84c7dd06480
Seshasai Kuchimanchi <seshasa@yahoo-inc.com>
2015-02-20 04:27:04 -0800

    Just added some code for the required functionality. The code should be moved to respective places

--- app/src/main/java/com/findbuddy/findbuddy/MapViewFragment.java
+++ app/src/main/java/com/findbuddy/findbuddy/MapViewFragment.java
@@ -1,16 +1,33 @@
 package com.findbuddy.findbuddy;
 
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.provider.SyncStateContract;
+import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.findbuddy.findbuddy.models.User;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * Created by abhidhar on 2/19/15.
@@ -18,6 +35,8 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class MapViewFragment extends com.google.android.gms.maps.SupportMapFragment {
 
     private GoogleMap mMap;
+    private HashMap<String ,User> markerUsers = null;
+    private ArrayList<User> users = null;
 
     public  MapViewFragment() {
         super();
@@ -33,6 +52,7 @@ public class MapViewFragment extends com.google.android.gms.maps.SupportMapFragm
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //setUpMapIfNeeded();
+        markerUsers = new HashMap<>();
     }
 
     @Override
@@ -41,6 +61,12 @@ public class MapViewFragment extends com.google.android.gms.maps.SupportMapFragm
         setUpMapIfNeeded();
 
         return v;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
     }
 
     /**
@@ -78,9 +104,101 @@ public class MapViewFragment extends com.google.android.gms.maps.SupportMapFragm
      * This should only be called once and when we are sure that {@link #mMap} is not null.
      */
     private void setUpMap() {
-        mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
-    }
+        //mMap.addMarker(new MarkerOptions().position(new LatLng(37.2, -121.4)).title("Marker"));
+        mMap.setMyLocationEnabled(true);
 
+        setUpCustomInfoWidget();
+        users = User.getDummyValues();
+
+        Marker marker;
+        for(int i = 0; i<users.size();i++)
+        {
+            MarkerOptions markerOptions = new MarkerOptions().position(new LatLng(users.get(i).getLat(), users.get(i).getLon()));
+            markerOptions.icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_pin));
+            Bitmap bitmap;
+
+            marker = mMap.addMarker(markerOptions);
+            markerUsers.put(marker.getId(),users.get(i));
+        }
+        mMap.setOnMapLoadedCallback(new GoogleMap.OnMapLoadedCallback() {
+            @Override
+            public void onMapLoaded() {
+                setUpLocationsService();
+            }
+        });
+    }
+    private void setUpLocationsService()
+    {
+        LocationManager locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+        Criteria criteria = new Criteria();
+
+        Location location = locationManager.getLastKnownLocation(locationManager.getBestProvider(criteria, false));
+
+        if (location != null)
+        {
+            Double minLat = 90.0, minLon = 180.0, maxLat = -90.0,maxLon = -180.0;
+
+            for(int i =0; i< users.size();i++)
+            {
+                User user = users.get(i);
+                if(minLat > user.getLat())
+                    minLat = user.getLat();
+                if(minLon > user.getLon())
+                    minLon = user.getLon();
+                if(maxLat < user.getLat())
+                    maxLat = user.getLat();
+                if(maxLon < user.getLon())
+                    maxLon = user.getLon();
+            }
+            if(minLat > location.getLatitude())
+                minLat = location.getLatitude();
+            if(minLon > location.getLongitude())
+                minLon = location.getLongitude();
+            if(maxLat < location.getLatitude())
+                maxLat = location.getLatitude();
+            if(maxLon < location.getLongitude())
+                maxLon = location.getLongitude();
+            LatLngBounds bounds= new LatLngBounds( new LatLng(minLat,minLon),new LatLng(maxLat,maxLon));
+            mMap.moveCamera(CameraUpdateFactory.newLatLngBounds(bounds, 100));
+            //mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(location.getLatitude(), location.getLongitude()), 5));
+        }
+    }
+    private void setUpCustomInfoWidget()
+    {
+        mMap.setInfoWindowAdapter(new GoogleMap.InfoWindowAdapter() {
+
+            @Override
+            public View getInfoContents(Marker args) {
+                return null;
+            }
+
+            // Defines the contents of the InfoWindow
+            @Override
+            public View getInfoWindow(Marker args) {
+
+                // Getting view from the layout file info_window_layout
+                View v = getActivity().getLayoutInflater().inflate(R.layout.info_widget_layout, null);
+                TextView tvUserName = (TextView) v.findViewById(R.id.tvUserName);
+                TextView tvUserId = (TextView)v.findViewById(R.id.tvUserId);
+                TextView tvUpdateTime = (TextView)v.findViewById(R.id.tvUpdateTime);
+                User user = markerUsers.get(args.getId());
+                tvUserName.setText("Name: " + user.getName());
+                tvUserId.setText("ID: " + user.getId());
+                tvUpdateTime.setText("Last Updated:" + user.getName());
+
+                mMap.setOnInfoWindowClickListener(new GoogleMap.OnInfoWindowClickListener() {
+                    public void onInfoWindowClick(Marker marker) {
+
+                        Toast.makeText(getActivity(), "Clicked on Info Widget", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+                // Returning the view containing InfoWindow contents
+                return v;
+
+            }
+        });
+    }
     public void onZoomInClick(View view) {
         //CameraPosition cameraPosition = new CameraPosition.Builder().target(new LatLng(17.385044, 78.486671)).zoom(21).build();
         //mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
--- app/src/main/java/com/findbuddy/findbuddy/models/user.java
+++ app/src/main/java/com/findbuddy/findbuddy/models/user.java
@@ -1,7 +1,56 @@
 package com.findbuddy.findbuddy.models;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 /**
  * Created by seshasa on 2/20/15.
  */
 public class User {
+    private String id;
+    private String name;
+    private Double lat;
+    private Double lon;
+    private float accuracy;
+
+    public User(String id, String name, Double lat, Double lon, float accuracy) {
+        this.id = id;
+        this.name = name;
+        this.lat = lat;
+        this.lon = lon;
+        this.accuracy = accuracy;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Double getLat() {
+        return lat;
+    }
+
+    public Double getLon() {
+        return lon;
+    }
+
+    public float getAccuracy() {
+        return accuracy;
+    }
+
+    public static ArrayList<User> getDummyValues()
+    {
+        Double longs[] = {-121.82,-121.92,-122.02,-122.12,-122.22};
+        Double lats[] = {37.2,37.25,37.3,37.35,37.4};
+        ArrayList<User> users = new ArrayList<>();
+        for(int i = 1; i <= 5; i++)
+        {
+            users.add(new User(String.valueOf(i),"user "+String.valueOf(i),lats[i-1],longs[i-1],500));
+        }
+        return users;
+
+    }
 }
