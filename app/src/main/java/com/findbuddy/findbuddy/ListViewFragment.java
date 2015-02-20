7037cc826018ffb4c2b541dcbc4088bf80e2779e
abhidhar <abhidhar@yahoo-inc.com>
2015-02-19 23:24:05 -0800

    trying to add drawer with map

---
+++ app/src/main/java/com/findbuddy/findbuddy/ListViewFragment.java
@@ -0,0 +1,9 @@
+package com.findbuddy.findbuddy;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by abhidhar on 2/19/15.
+ */
+public class ListViewFragment extends Fragment {
+}
---
+++ app/src/main/java/com/findbuddy/findbuddy/OnFragmentInteractionListener.java
@@ -0,0 +1,9 @@
+package com.findbuddy.findbuddy;
+
+/**
+ * Created by abhidhar on 2/19/15.
+ */
+public interface OnFragmentInteractionListener {
+    // TODO: Update argument type and name
+    public void onFragmentInteraction(String id);
+}
